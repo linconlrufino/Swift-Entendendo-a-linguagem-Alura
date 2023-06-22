9286da1b428944c490890d327bb91e7a0afa8472
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-06-21 22:57:14 -0300

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+/*var nome: String = "Linconl"
+let idade: Int = 24
+
+print("O meu nome é \(nome)")
+print(idade)
+
+var coeficienteRendimento: Double = 9.5
+var possuiHistoricoReprovacao: Bool = false
+
+var turnoEmQueEstuda: Character = "D"
+
+typealias Estudante = String
+
+var aluno1: Estudante = "João"
+var aluno2: Estudante = "Ana"*/
+
+/*var letraInicialNome: Character = "L"
+var nome: String = "Linconl"
+var apelido: String = "Linkolt"
+var idade: Int = 24
+var altura: Double = 1.74
+var gostaOuNao: Bool = true
+
+print("Inicial do nome: \(letraInicialNome) \nNome: \(nome) \nApelido: \(apelido) \nIdade: \(idade) \nAltura: \(altura) \nGosta de Gatos: \(gostaOuNao)")
+*/
