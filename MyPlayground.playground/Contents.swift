0eabb99beb363be1c139c04ab900070df658527c
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-06-22 17:04:08 -0300

    Operações aritméticas e de atribuição

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -23,3 +23,31 @@ var gostaOuNao: Bool = true
 
 print("Inicial do nome: \(letraInicialNome) \nNome: \(nome) \nApelido: \(apelido) \nIdade: \(idade) \nAltura: \(altura) \nGosta de Gatos: \(gostaOuNao)")
 */
+
+10 + 2
+10 - 2
+8 * 2
+10 / 2
+
+5 / 2
+5.0 / 2
+Double(5) / 2
+
+5 % 2
+5 + 2 * 10
+(5 + 2) * 10
+
+
+var numero1 = 10
+var numero2 = 50
+var soma = numero1 + numero2
+print(soma)
+
+numero1 += 2
+numero1 *= 2
+numero1 -= 2
+
+var nome = "Linconl"
+var sobrenome = "Castro"
+print(nome + sobrenome)
+print("Meu nome é \(nome) \(sobrenome)")
